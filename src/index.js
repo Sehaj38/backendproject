a05297321cc0041a602f2a761c15fc641f3f018c
Sehaj38 <kaursehajneet06@gmail.com>
2026-07-16 17:11:45 +0530

    utilities code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 //require('dotenv').config({path: '/.env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js"
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
-connectDB();
+
+connectDB()
+.then(()=>{
+    try{
+        app.on("error", (error)=>{
+            console.log('Error: ', error);
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, ()=>{
+            console.log(`Server is running at : ${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.error("Error: ", error);
+        throw error
+    }
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed!", err);
+})
 
 
 
